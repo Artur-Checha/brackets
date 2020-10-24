1212df7bef0e7fe069edaa263285f06840f2516c
Artur-Checha <artur.energetic@gmail.com>
2020-10-24 23:29:12 +0300

    feat: function changed 2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-export default function check(str, bracketsConfig) {
-  let o = Object.fromEntries(bracketsConfig);
-  let stack = [];
-  for (let i = 0; i < str.length; ++i) {
-      if (stack[stack.length - 1] === str[i]) {
-          stack.pop();
-      } else if (o[str[i]]) {
-          stack.push(o[str[i]]);
-      } else {
-          return false;
-      }
-  }
-  return !stack.length;
+module.exports = function check(str, bracketsConfig) {
+    let o = Object.fromEntries(bracketsConfig);
+    let stack = [];
+    for (let i = 0; i < str.length; ++i) {
+        if (stack[stack.length - 1] === str[i]) {
+            stack.pop();
+        } else if (o[str[i]]) {
+            stack.push(o[str[i]]);
+        } else {
+            return false;
+        }
+    }
+    return !stack.length;
 }
