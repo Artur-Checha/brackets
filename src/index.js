28807b1e8c4269957ac9abf98118a8b2a315f3a2
Artur-Checha <artur.energetic@gmail.com>
2020-10-24 23:22:49 +0300

    feat: function changed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+export default function check(str, bracketsConfig) {
+  let o = Object.fromEntries(bracketsConfig);
+  let stack = [];
+  for (let i = 0; i < str.length; ++i) {
+      if (stack[stack.length - 1] === str[i]) {
+          stack.pop();
+      } else if (o[str[i]]) {
+          stack.push(o[str[i]]);
+      } else {
+          return false;
+      }
+  }
+  return !stack.length;
 }
